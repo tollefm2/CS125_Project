f3ce53a1233c47b90aafefe54ec3e877ef0de809
Margo Tollefson <tollefm2@my.erau.edu>
2023-03-09 22:57:33 -0700

    start screen

--- theOregonTrail.c
+++ theOregonTrail.c
@@ -1,7 +1,22 @@
+/*****
+*File Name: theOregonTrail.c
+*Collaborators: Sam Costello, Fjor Robles, Margo Tollefson
+*Purpose: Simulate Oregon Trail type game
+*****/
+
 #include <stdio.h>
 
 int main () {
-	printf("GIT Test.\n");
+	char start;
+
+	printf("WELCOME TO THE OREGON TRAIL!\n\n");
+	
+	printf("Press 'y' to start:");
+	scanf(" %c", &start);
+
+	while (start == 'y') {
+		printf("test.\n");
+	}
 
 	return 0;
 }
