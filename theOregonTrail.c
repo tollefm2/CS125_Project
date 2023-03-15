ca29c8f92a3cbc3e91c9c1313a111073abab5311
Margo Tollefson <tollefm2@my.erau.edu>
2023-03-15 10:36:30 -0700

    occupation

--- theOregonTrail.c
+++ theOregonTrail.c
@@ -6,6 +6,24 @@
 
 #include <stdio.h>
 
+void userOccupation () {
+	int occupation;
+
+	printf("Before you start the trail you must choose an occupation: \n1. Farmer\n2. Banker\n3. Carpenter \nWhat occupation would you like to choose? ");
+	scanf("%d", &occupation);
+
+	switch (occupation) {
+		case 1:
+			printf("You have chosen farmer.\n");
+			break;
+		case 2:
+			printf("You have chosen banker.\n");
+			break;
+		case 3:
+			printf("You have chosen carpenter.\n");
+			break;
+	}
+}
 int main () {
 	char start;
 
@@ -15,7 +33,7 @@ int main () {
 	scanf(" %c", &start);
 
 	while (start == 'y') {
-		printf("test.\n");
+		userOccupation();
 	}
 
 	return 0;
