81fabf990746390b3cda67eb8180469840c07312
Margo Tollefson <tollefm2@my.erau.edu>
2023-03-21 10:27:04 -0700

    more stuff

--- theOregonTrail.c
+++ theOregonTrail.c
@@ -5,6 +5,8 @@
 *****/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void userOccupation () {
 	int occupation;
@@ -24,9 +26,19 @@ void userOccupation () {
 			break;
 	}
 }
-int main () {
-	char start;
 
+struct family {
+	char name[10];
+	int memHealth;
+};
+
+int main () {
+	int memHealth = 100;
+	int day = 1;
+	char start;
+	int memNum;
+	struct family partyMember[4];
+	
 	printf("WELCOME TO THE OREGON TRAIL!\n\n");
 	
 	printf("Press 'y' to start:");
@@ -34,6 +46,22 @@ int main () {
 
 	while (start == 'y') {
 		userOccupation();
+		printf("Before you start the trail, you must name your party members.\n");
+		
+		for (memNum = 1; memNum < 4; memNum++) {
+			printf("Party member %d: ", memNum);
+			scanf("%s", partyMember[memNum].name);
+			
+			if (strlen(partyMember[memNum].name) > 10) {
+				printf("Character length exceeded. Please try again.\n");
+				memNum--;
+			}
+			
+		}
+		
+		printf("DAY %d\n", day);
+		printf("%d", partyMember[1].memHealth);
+	return 0;
 	}
 
 	return 0;
