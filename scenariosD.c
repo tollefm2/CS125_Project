5d1db714ff0d777c300a92f2fce0be544042f786
catandladybug <98188215+catandladybug@users.noreply.github.com>
2023-04-03 13:36:16 -0700

    Update scenariosD.c

--- scenariosD.c
+++ scenariosD.c
@@ -1,3 +1,10 @@
+/*****
+*File Name: scenariosD.c
+*Collaborators: Sam Costello, Fjor Robles, Margo Tollefson
+*Main Contributers for this file: Sam Costello
+*Purpose: Holds events that deal with days
+*****/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
