fab6a74a2968735137a6481a6c064c45399d87a8
catandladybug <98188215+catandladybug@users.noreply.github.com>
2023-04-03 13:37:44 -0700

    Update scenariosL.c

--- scenariosL.c
+++ scenariosL.c
@@ -1,5 +1,5 @@
 /*****
-*File Name: scenarios.c
+*File Name: scenariosL.c
 *Collaborators: Sam Costello, Fjor Robles, Margo Tollefson
 *Main Contributers for this file: Sam Costello
 *Purpose: Holds events that deal with lives
