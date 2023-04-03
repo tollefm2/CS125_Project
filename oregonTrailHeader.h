9011275813ab6a22669eaca630f3f19ca757add4
catandladybug <98188215+catandladybug@users.noreply.github.com>
2023-04-03 13:37:15 -0700

    Update oregonTrailHeader.h

--- oregonTrailHeader.h
+++ oregonTrailHeader.h
@@ -1,3 +1,10 @@
+/*****
+*File Name: oregonTrailHeader.h
+*Collaborators: Sam Costello, Fjor Robles, Margo Tollefson
+*Main Contributers for this file: Margo Tollefson
+*Purpose: Defines files to use in main code
+*****/
+
 int scenarioD(int);
 
 int scenariosH(int);
